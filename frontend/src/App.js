114212a70c5bc69db73c842b05eefd79d9aeb472
Harshit Khanna <harshitkhanna006@gmail.com>
2025-07-31 14:57:14 +0530

    fixed error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,27 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
-import LoginPage from "./Login";
-import HomePage from './HomePage';
-import ProtectedRoute from "./ProtectedRoute";
-import RegisterPage from "./Register";
-import OAuthLogin from "./OAuthLogin";
-import Logout from "./Logout";
-
+import WhatsHappening from "./components/WhatsHappening";
+import Navbar from "./components/Navbar";
+import Features from "./components/Features";
+import Hero from "./components/Hero";
+import Testimonials from "./components/Testimonials";
+import Community from "./components/Community";
+import Footer from "./components/Footer";
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/login" element={<LoginPage/>}/>
-                <Route path="/register" element={<RegisterPage/>}/>
-                <Route path="/" element={<ProtectedRoute><HomePage/></ProtectedRoute>}/>
-                <Route path="/oauth-login" element={<OAuthLogin/>}> </Route>
-                <Route path="/logout" element={<Logout/>}> </Route>
-
-                //enclose any page you want to protect with protected route
-            </Routes>
-
-
-        </Router>
-    );
+        <>
+            <div className="App">
+                <Navbar/>
+                <main>
+                    <Hero/>
+                    <WhatsHappening/>
+                    <Features/>
+                    <Testimonials/>
+                    <Community/>
+                </main>
+                <Footer/>
+            </div>
+        </>);
 }
 
 export default App;
