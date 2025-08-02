3c05274f9f8aadd44936b21a181abcdbd382ae8f
Sandeep Vashishtha <sandeepvashishtha@outlook.in>
2025-08-02 17:13:52 +0530

    Revert "implemented cookie based jwt and spring security for authentication"

--- backend/src/main/java/com/eventra/Dto/UserDto.java
+++
@@ -1,9 +0,0 @@
-package com.eventra.Dto;
-
-import lombok.Data;
-
-@Data
-public class UserDto {
-    String email;
-    String password;
-}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,50 @@
-
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import WhatsHappening from './components/WhatsHappening';
+import Features from './components/Features';
+import Testimonials from './components/Testimonials';
+import Community from './components/Community';
+import Footer from './components/Footer';
+import EventCreation from './components/common/EventCreation';
+import AboutPage from './components/AboutPage';
+import createScrollToTopButton from './components/scrolltotopButton';
 import './App.css';
-import WhatsHappening from "./components/WhatsHappening";
-import Navbar from "./components/Navbar";
-import Features from "./components/Features";
-import Hero from "./components/Hero";
-import Testimonials from "./components/Testimonials";
-import Community from "./components/Community";
-import Footer from "./components/Footer";
-function App() {
-    return (
-        <>
-            <div className="App">
-                <Navbar/>
-                <main>
-                    <Hero/>
-                    <WhatsHappening/>
-                    <Features/>
-                    <Testimonials/>
-                    <Community/>
-                </main>
-                <Footer/>
-            </div>
-        </>);
 
+function App() {
+  useEffect(() => {
+    createScrollToTopButton(); 
+  }, []);
+
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+
+        <main>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Hero />
+                  <WhatsHappening />
+                  <Features />
+                  <Testimonials />
+                  <Community />
+                </>
+              }
+            />
+            <Route path="/create-event" element={<EventCreation />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
