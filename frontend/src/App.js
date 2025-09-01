f945a3b81e1621734c8b1792d80871452248e826
Sandeep Vashishtha <sandeepvashishtha@outlook.in>
2025-09-02 00:06:53 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import EventsPage from "./Pages/Events/EventsPage";
 import HackathonPage from "./Pages/Hackathons/HackathonPage";
 import ProjectsPage from "./Pages/Projects/ProjectsPage";
 import ContactUs from "./Pages/Contact/ContactUs"; // Import ContactUs page
+import LeaderBoard from "./components/Leaderboard";
 import NotFound from "./components/NotFound";
 
 // --------------- AUTH PAGES
@@ -42,6 +43,7 @@ function App() {
               <Route path="/hackathons" element={<HackathonPage />} />
               <Route path="/projects" element={<ProjectsPage />} />
               <Route path="/contributors" element={<Contributors />} />
+              <Route path="/leaderBoard" element={<LeaderBoard />} />
               <Route path="/about" element={<AboutPage />} />
               <Route path="/contact" element={<ContactUs />} />{" "}
               {/* Add ContactUs route */}
